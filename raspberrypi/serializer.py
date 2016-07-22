159c8b8a0b019312107cc539dea194c4bb0e477c
Jerhone <fjcamillo.dev@gmail.com>
2016-07-22 17:05:30 +0800

    test 1 for serial connection

--- raspberrypi/serializer.py
+++ raspberrypi/serializer.py
@@ -1,3 +1,11 @@
 def call_serial():
     import serial
 
+    arduino = serial.Serial('tty1', 115200, timeout=.1)
+
+    #DEBUGGING
+    while True:
+        arduino.readline()
+
+if __name__ == '__main__':
+    call_serial()
