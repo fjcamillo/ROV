1140683650e3ef30f9806e784c12c10ee8899d50
Jerhone <fjcamillo.dev@gmail.com>
2016-07-22 16:59:40 +0800

    creating serial connection within raspberry pi and arduino

--- raspberrypi/camera_detection.py
+++ raspberrypi/camera_detection.py
@@ -1,6 +1,33 @@
-import cv2
+def camera_detect(facexmls, eyesxmls, camera):
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    import cv2
 
-cap = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(facexmls)
+    eye_cascade = cv2.CascadeClassifier(eyesxmls)
+
+    cap = cv2.VideoCapture(camera)
+
+    while True:
+        ret, img = cap.read()
+        convert_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(convert_gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w, y+h), (255,0,0),2)
+            roi_gray = convert_gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color,(ex, ey), (ex+ew, ey+eh),(0,255,0),2)
+
+        cv2.imshow('image', img)
+
+        k = cv2.waitKey(30) & 0xFF
+        if k==27:
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+if __name__ == '__main__':
+    camera_detect()
---
+++ raspberrypi/serializer.py
@@ -0,0 +1,3 @@
+def call_serial():
+    import serial
+
