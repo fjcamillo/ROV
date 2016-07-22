dcf9852e27f715b167a17594e7de51d910d88d19
Jerhone <fjcamillo.dev@gmail.com>
2016-07-22 15:13:18 +0800

    continued code for camera

--- raspberrypi/camera_detection.py
+++ raspberrypi/camera_detection.py
@@ -1,4 +1,6 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_casc    
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
