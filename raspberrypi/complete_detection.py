1933651899ccbfaffd8c40ffd260f02b54d1b0c1
Jerhone <fjcamillo.dev@gmail.com>
2016-07-23 05:57:58 +0800

    missed a command

--- raspberrypi/complete_detection.py
+++ raspberrypi/complete_detection.py
@@ -10,7 +10,7 @@ def complete_detect(facexmls, eyesxmls, bodyxmls, camera):
     while True:
 
         ret, img = cap.read()
-        convert_image_to_gray = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
+        convert_image_to_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(convert_image_to_gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2. rectangle(img, (x, y), (x+w, y+h), (255, 0,0), 2)
